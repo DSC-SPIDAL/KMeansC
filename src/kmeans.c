e462268afc29fd98c2203983b5722ea762cb9783
Saliya Ekanayake <sekanaya@indiana.edu>
2016-06-26 16:29:42 -0400

    partial kmeans

--- src/kmeans.c
+++ src/kmeans.c
@@ -3,7 +3,17 @@
 #include <stdlib.h>
 #include <unistd.h>
 #include <mpi.h>
-int parse_args(int, char **);
+#include <float.h>
+#include <math.h>
+
+int parse_args(int argc, char **argv);
+void reset_array(double *array, int length);
+double euclidean_distance(double *points1, double* points2, int offset1, int offset2, int dim);
+int find_min_dist_center(double *points, double *centers, int num_centers, int dim, int points_offset);
+void find_nearest_centers(double *points, double *centers, int num_centers, int dim, double *center_sums_and_counts, int *cluster_assignments, int points_count, int points_start_idx, int offset);
+
+
+
 
 /* Configuration options */
 int num_points;
@@ -48,26 +58,27 @@ int main (int argc, char **argv)
   int p = num_points / world_procs_count;
   int q = num_points % world_procs_count;
   int proc_points_count = world_proc_rank < q ? p + 1 : p;
-  int proc_point_start_idx = world_proc_rank * p + (world_proc_rank < q ? world_proc_rank : q);
+  int proc_points_start_idx = world_proc_rank * p + (world_proc_rank < q ? world_proc_rank : q);
 
   /* Decompose points among threads */
   p = proc_points_count / num_threads;
   q = proc_points_count % num_threads;
   int thread_points_counts[num_threads];
-  int thread_point_start_idx[num_threads];
+  int thread_points_start_idx[num_threads];
   int i;
   for (i = 0; i < num_threads; ++i)
   {
     thread_points_counts[i] = i < q ? p+1 : p;
-    thread_point_start_idx[i] = (i * p + (i < q ? i : p));
+    thread_points_start_idx[i] = (i * p + (i < q ? i : p));
   }
 
   /* Read points and centers from files */
-  double *points = malloc(sizeof(double)*num_points*dim);
+  double *points = malloc(sizeof(double)*proc_points_count*dim);
   double *centers = malloc(sizeof(double)*num_centers*dim);
 
   FILE *f = fopen(points_file, "rb");
-  fread(points, sizeof(double), num_points*dim, f);
+  fseek(f,proc_points_start_idx*dim*sizeof(double), SEEK_SET);
+  fread(points, sizeof(double), proc_points_count*dim, f);
   fclose(f);
 
   f = fopen(centers_file, "rb");
@@ -77,7 +88,7 @@ int main (int argc, char **argv)
   /* Data structures for computation */
   int length_sums_and_counts = num_threads*num_centers*(dim+1);
   double thread_center_sums_and_counts[length_sums_and_counts];
-  double proc_cluster_assignments[proc_points_count];
+  int proc_cluster_assignments[proc_points_count];
 
   int itr_count = 0;
   int converged = 0;
@@ -88,14 +99,72 @@ int main (int argc, char **argv)
     ++itr_count;
     reset_array(thread_center_sums_and_counts, length_sums_and_counts);
 
+    if (num_threads > 1)
+    {
+      /* OpenMP parallel region */
+      // remember to send this offset threadIdx*numCenters*(dimension+1)
+    }
+    else
+    {
+      find_nearest_centers(points, centers, num_centers, dim, thread_center_sums_and_counts, proc_cluster_assignments, thread_points_counts[0], thread_points_start_idx[0], 0);
+    }
+
   }
 
   MPI_Finalize();
 
 }
 
+void find_nearest_centers(double *points, double *centers, int num_centers, int dim, double *center_sums_and_counts, int *cluster_assignments, int points_count, int points_start_idx, int offset)
+{
+  int i;
+  for (i = 0; i < points_count; ++i)
+  {
+    int points_offset = (points_start_idx + i) * dim;
+    int min_dist_center = find_min_dist_center(points, centers, num_centers, dim, points_offset);
+  
+  }
+  
+}
+
+int find_min_dist_center(double *points, double *centers, int num_centers, int dim, int points_offset)
+{
+  double min_d = DBL_MAX;
+  int min_d_idx = -1;
+  int i;
+  for (i = 0; i < num_centers; ++i)
+  {
+    double d = euclidean_distance(points, centers, points_offset, i*dim, dim);
+    if (d < min_d)
+    {
+      min_d = d;
+      min_d_idx = i;
+    }
+  }
+  return min_d_idx;
+}
+
+double euclidean_distance(double *points1, double* points2, int offset1, int offset2, int dim)
+{
+  double d = 0.0;
+  double tmp;
+  int i;
+  for (i = 0; i < dim; ++i)
+  {
+    tmp = points1[i+offset1] - points2[i+offset2];
+    d += tmp*tmp;
+  }
+  return sqrt(d);
+}
+
+
 void reset_array(double *array, int length)
 {
+  int i;
+  for (i = 0; i < length; ++i)
+  {
+    array[i] = 0.0;
+  }
 }
 
 int parse_args(int argc, char **argv)
