b68e9dd9c2be9e6298927ee3d6cbf0078a24a221
Saliya Ekanayake <sekanaya@indiana.edu>
2016-06-29 19:48:34 -0400

    adding results

--- src/run.all.sh
+++ src/run.all.sh
@@ -1,5 +1,6 @@
 #!/bin/bash
 
+verbose=
 #can be 1/0 to set thread pinning
 explicitbind=$1
 #can be core/socket/none
@@ -8,11 +9,11 @@ nodes=16
 name="$nodes"n
 nodefile=nodes.$name.txt
 
-./run.generic.sh 1 24 $nodefile $nodes $explicitbind $procbind
-#./run.generic.sh 2 12 $nodefile $nodes $explicitbind $procbind
-#./run.generic.sh 3 8 $nodefile $nodes $explicitbind $procbind
-#./run.generic.sh 4 6 $nodefile $nodes $explicitbind $procbind
-#./run.generic.sh 6 4 $nodefile $nodes $explicitbind $procbind
-#./run.generic.sh 8 3 $nodefile $nodes $explicitbind $procbind
-#./run.generic.sh 12 2 $nodefile $nodes $explicitbind $procbind
-#./run.generic.sh 24 1 $nodefile $nodes $explicitbind $procbind
+./run.generic.sh 1 24 $nodefile $nodes $explicitbind $procbind $verbose
+./run.generic.sh 2 12 $nodefile $nodes $explicitbind $procbind $verbose
+./run.generic.sh 3 8 $nodefile $nodes $explicitbind $procbind $verbose
+./run.generic.sh 4 6 $nodefile $nodes $explicitbind $procbind $verbose
+./run.generic.sh 6 4 $nodefile $nodes $explicitbind $procbind $verbose
+./run.generic.sh 8 3 $nodefile $nodes $explicitbind $procbind $verbose
+./run.generic.sh 12 2 $nodefile $nodes $explicitbind $procbind $verbose
+./run.generic.sh 24 1 $nodefile $nodes $explicitbind $procbind $verbose
