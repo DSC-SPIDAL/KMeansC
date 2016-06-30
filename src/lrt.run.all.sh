3b7629d32149d6f8e4ac24cc85d9f34afcb02db8
Saliya Ekanayake <sekanaya@indiana.edu>
2016-06-30 16:28:24 -0400

    adding scripts

---
+++ src/lrt.run.all.sh
@@ -0,0 +1,18 @@
+#!/bin/bash
+
+#can be 1/0 to set thread pinning
+explicitbind=$1
+#can be core/socket/none
+procbind=$2
+nodes=16
+name="$nodes"n
+nodefile=nodes.$name.txt
+
+./lrt.run.generic.sh 1 24 $nodefile $nodes $explicitbind $procbind
+#./lrt.run.generic.sh 2 12 $nodefile $nodes $explicitbind $procbind
+#./lrt.run.generic.sh 3 8 $nodefile $nodes $explicitbind $procbind
+#./lrt.run.generic.sh 4 6 $nodefile $nodes $explicitbind $procbind
+#./lrt.run.generic.sh 6 4 $nodefile $nodes $explicitbind $procbind
+#./lrt.run.generic.sh 8 3 $nodefile $nodes $explicitbind $procbind
+#./lrt.run.generic.sh 12 2 $nodefile $nodes $explicitbind $procbind
+#./lrt.run.generic.sh 24 1 $nodefile $nodes $explicitbind $procbind
