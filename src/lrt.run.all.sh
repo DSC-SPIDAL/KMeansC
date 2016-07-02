5ae11fd2aeb23b310a2674788066a616a3a18148
Saliya Ekanayake <sekanaya@indiana.edu>
2016-07-02 13:14:10 -0400

    adding results

--- src/lrt.run.all.sh
+++ src/lrt.run.all.sh
@@ -9,11 +9,11 @@ nodes=16
 name="$nodes"n
 nodefile=nodes.$name.txt
 
-./run.generic.sh 1 24 $nodefile $nodes $explicitbind $procbind $verbose
-./run.generic.sh 2 12 $nodefile $nodes $explicitbind $procbind $verbose
-./run.generic.sh 3 8 $nodefile $nodes $explicitbind $procbind $verbose
-./run.generic.sh 4 6 $nodefile $nodes $explicitbind $procbind $verbose
-./run.generic.sh 6 4 $nodefile $nodes $explicitbind $procbind $verbose
-./run.generic.sh 8 3 $nodefile $nodes $explicitbind $procbind $verbose
-./run.generic.sh 12 2 $nodefile $nodes $explicitbind $procbind $verbose
-./run.generic.sh 24 1 $nodefile $nodes $explicitbind $procbind $verbose
+./lrt.run.generic.sh 1 24 $nodefile $nodes $explicitbind $procbind $verbose
+./lrt.run.generic.sh 2 12 $nodefile $nodes $explicitbind $procbind $verbose
+./lrt.run.generic.sh 3 8 $nodefile $nodes $explicitbind $procbind $verbose
+./lrt.run.generic.sh 4 6 $nodefile $nodes $explicitbind $procbind $verbose
+./lrt.run.generic.sh 6 4 $nodefile $nodes $explicitbind $procbind $verbose
+./lrt.run.generic.sh 8 3 $nodefile $nodes $explicitbind $procbind $verbose
+./lrt.run.generic.sh 12 2 $nodefile $nodes $explicitbind $procbind $verbose
+./lrt.run.generic.sh 24 1 $nodefile $nodes $explicitbind $procbind $verbose
--- src/lrt.small.run.all.sh
+++ src/lrt.small.run.all.sh
@@ -9,16 +9,16 @@ nodes=1
 name="$nodes"n
 nodefile=nodes.$name.txt
 
-#./small.run.generic.sh 1 2 $nodefile $nodes $explicitbind $procbind $verbose
-./small.run.generic.sh 1 24 $nodefile $nodes $explicitbind $procbind $verbose
+#./lrt.small.run.generic.sh 1 2 $nodefile $nodes $explicitbind $procbind $verbose
+./lrt.small.run.generic.sh 1 24 $nodefile $nodes $explicitbind $procbind $verbose
 :<<COMMENT
-./small.run.generic.sh 2 12 $nodefile $nodes $explicitbind $procbind $verbose
-./small.run.generic.sh 3 8 $nodefile $nodes $explicitbind $procbind $verbose
+./lrt.small.run.generic.sh 2 12 $nodefile $nodes $explicitbind $procbind $verbose
+./lrt.small.run.generic.sh 3 8 $nodefile $nodes $explicitbind $procbind $verbose
 COMMENT
-#./small.run.generic.sh 4 6 $nodefile $nodes $explicitbind $procbind $verbose
+#./lrt.small.run.generic.sh 4 6 $nodefile $nodes $explicitbind $procbind $verbose
 :<<COMMENT
-./small.run.generic.sh 6 4 $nodefile $nodes $explicitbind $procbind $verbose
-./small.run.generic.sh 8 3 $nodefile $nodes $explicitbind $procbind $verbose
-./small.run.generic.sh 12 2 $nodefile $nodes $explicitbind $procbind $verbose
+./lrt.small.run.generic.sh 6 4 $nodefile $nodes $explicitbind $procbind $verbose
+./lrt.small.run.generic.sh 8 3 $nodefile $nodes $explicitbind $procbind $verbose
+./lrt.small.run.generic.sh 12 2 $nodefile $nodes $explicitbind $procbind $verbose
 COMMENT
-#./small.run.generic.sh 24 1 $nodefile $nodes $explicitbind $procbind $verbose
+#./lrt.small.run.generic.sh 24 1 $nodefile $nodes $explicitbind $procbind $verbose
